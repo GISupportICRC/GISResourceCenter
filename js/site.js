d95d77962ed95d075c4b6edaca5242e06a135a33
Yann Rebois <yrebois@icrc.org>
2018-01-16 17:06:00 +0100

    Updated

--- js/site.js
+++ js/site.js
@@ -1,6 +1,7 @@
 
 window.onload = function() {
 	
+
 	// *** START - load map from cartodb ***
 	/*
 	cartodb.createVis('map', 'https://icrc.cartodb.com/api/v2/viz/2c05f200-3ff3-11e3-a2b1-3085a9a9563c/viz.json', {
